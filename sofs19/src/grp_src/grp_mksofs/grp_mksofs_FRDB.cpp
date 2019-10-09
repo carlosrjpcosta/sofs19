2c073b329748eba517fca11f17071b5790b930ea
Ricardo Carvalho <rapc99@gmail.com>
2019-10-09 04:32:40 +0100

    FillReferenceDataBlocks (Complete)

--- sofs19/src/grp_src/grp_mksofs/grp_mksofs_FRDB.cpp
+++ sofs19/src/grp_src/grp_mksofs/grp_mksofs_FRDB.cpp
@@ -13,8 +13,31 @@ namespace sofs19
     {
         soProbe(605, "%s(%u, %u, %u)\n", __FUNCTION__, ntotal, itotal, nbref);
 
-        /* change the following line by your code */
-        return binFillReferenceDataBlocks(ntotal, itotal, nbref);
+        //RPB -- References per block
+        if(nbref==0)
+            return; //Every reference is in the head cache
+        uint32_t nextReference= (nbref + 1) + HEAD_CACHE_SIZE; //Block 0 is for the root         
+        uint32_t totalDataBlocks= ntotal - (itotal/IPB); //Number of Data Blocks
+        for(uint32_t i=0; i<nbref; i++){ //Goes through all Reference Data Blocks (nbref)
+            uint32_t buf [RPB];
+            if(i==nbref-1) // Last Reference Block
+                buf[0]= NullReference;
+            else
+                buf[0]= i+2;     
+            for(uint32_t j=1; j<RPB; j++){
+                if(nextReference==totalDataBlocks-1){ // No more Data Blocks
+                    for(uint32_t k=j;k<RPB;k++){
+                        buf[k]= NullReference;
+                    }
+                    break;
+                }  
+                else{
+                    buf[j]= nextReference;
+                    nextReference++;
+                }            
+            }
+            soWriteRawBlock(i+2+(itotal/IPB),&buf); // Writes in the disk the buffer
+        }
     }
 };
 
