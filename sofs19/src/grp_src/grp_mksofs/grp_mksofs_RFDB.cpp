eced287402be9b389a7df817f3ed67abd18b4d58
Carlos Costa <carlospalmacosta@ua.pt>
2019-10-13 12:46:46 +0100

    Função soResetFreeDataBlocks

--- sofs19/src/grp_src/grp_mksofs/grp_mksofs_RFDB.cpp
+++ sofs19/src/grp_src/grp_mksofs/grp_mksofs_RFDB.cpp
@@ -14,7 +14,13 @@ namespace sofs19
         soProbe(607, "%s(%u, %u, %u)\n", __FUNCTION__, ntotal, itotal, nbref);
 
         /* change the following line by your code */
-        binResetFreeDataBlocks(ntotal, itotal, nbref);
+        //binResetFreeDataBlocks(ntotal, itotal, nbref);
+        uint32_t array [BlockSize];
+        memset(array, 0, BlockSize);
+        uint32_t freeblocks = ntotal - IPB - nbref - 2; //O número de blocos livres são o número total de blocos menos os blocos ocupados (o superblock, os da tabela de inodes, os de referência (se existirem) e a raiz)
+        for(uint32_t i = 0; i < freeblocks; i++){
+            soWriteRawBlock(i, &array);
+        }
     }
 };
 
