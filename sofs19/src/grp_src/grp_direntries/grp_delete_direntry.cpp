f28044e4d0c4bca6590614ac2ebf2d359c9e24ce
Ricardo Carvalho <rapc99@gmail.com>
2019-11-11 09:27:42 +0000

    DeleteDirentry (Complete)

--- sofs19/src/grp_src/grp_direntries/grp_delete_direntry.cpp
+++ sofs19/src/grp_src/grp_direntries/grp_delete_direntry.cpp
@@ -15,8 +15,28 @@ namespace sofs19
     {
         soProbe(203, "%s(%d, %s)\n", __FUNCTION__, pih, name);
 
-        /* change the following line by your code */
-        return binDeleteDirEntry(pih, name);
+        //soCheckInodeHandler(pih, __FUNCTION__);
+        //No need to check Parent Inode Handler (pih)
+        //It is assumed that pih is a valid handler of a inode where the user has write access
+        SOInode* inode= soGetInodePointer(pih);
+        SODirEntry deblk[DPB];
+        uint32_t tfb= inode->size / BlockSize; // Total number of file blocks
+        
+        for (uint32_t fbn= 0; fbn<tfb; fbn++){
+            soReadFileBlock (pih, fbn, deblk);
+            for (uint32_t de= 0; de<DPB; de++){
+                uint32_t din= deblk[de].in;
+                if (strcmp(deblk[de].name, name)==0){
+                    char tmp= deblk[de].name[0];
+                    deblk[de].name[0]= '\0';
+                    deblk[de].name[SOFS19_MAX_NAME]= tmp;
+                    deblk[de].in= NullReference;
+                    soWriteFileBlock(pih,fbn,deblk);
+                    return din;    
+                }
+            }
+        }
+        throw SOException(ENOENT,__FUNCTION__); // Name does not exist
     }
 };
 
